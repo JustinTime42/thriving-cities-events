9ddb6a6263301a78918fccf39a4818577bc06a57
Justin Schneider <justincase18@msn.com>
2018-05-16 11:05:59 -0800

    navbar added

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -6,28 +6,31 @@ const NavBar = () => {
         let burgerID = document.getElementById("burgerButton")
         burgerID.classList.toggle("is-active")
         if (burgerID.classList.contains("is-active")){            
-            document.getElementById("test").style.height="100%";
+            document.getElementById("test").style.height="100%"
+            
         } else {
-            document.getElementById("test").style.height="40px";
+            document.getElementById("test").style.height="40px"
+           
         }
     }
     return (
         <header id="test" className="topnav">
-            
+            <h3>Thriving Cities Events</h3>
+            <div>
             <button id="burgerButton" className="hamburger hamburger--spin" type="button" onClick={onClick}>
                 <span className="hamburger-box">
                     <span className="hamburger-inner"></span>
-                </span>
-            </button>  
-            <nav>
-                <div>
-                    <a href="http://http://thrivingcities.com/">Thriving Cities Homepage</a>
-                </div>
-                <div>
-                    <a href="http://http://thrivingcities.com/">Thriving Cities Homepage2</a>
-                </div>
-
-            </nav>
+                </span>              
+                <nav>
+                    <div>
+                        <a href="http://thrivingcities.com/">Thriving Cities Homepage</a>
+                    </div>
+                    <div>
+                        <a href="http://thrivingcities.com/">Thriving Cities Homepage2</a>
+                    </div>
+                </nav>
+            </button>
+            </div>
         </header>
     )
 }
